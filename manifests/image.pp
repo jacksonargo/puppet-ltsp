90a4ebea63377ccc5a08f218b0c1103656b6e3dc
Jackson Argo <ignition.argo@gmail.com>
2015-01-23 17:42:08 -0500

    Cleaned up style.

--- manifests/image.pp
+++ manifests/image.pp
@@ -1,56 +1,56 @@
-/* ltsp/manifests/client.pp
- *
- * Resource to manage ltsp client images
- * Creates a chroot image for the server.
- * Runs puppet from inside chroot to configure it.
- */
+# ltsp/manifests/client.pp
+#
+# Resource to manage ltsp client images
+# Creates a chroot image for the server.
+# Runs puppet from inside chroot to configure it.
+#
 
 define ltsp::image (
-    $image = $title,
     $image_cert,
-    $image_environment = $environment,
-    $image_puppetmater = $servername,
-    $image_puppetmasterip = $serverip,
+    $image = $title,
+    $image_environment = $::environment,
+    $image_puppetmater = $::servername,
+    $image_puppetmasterip = $::serverip,
 ) {
 
-    $chroot = "/opt/ltsp/$image"
+    $chroot = "/opt/ltsp/${image}"
 
     # First we need to tell ltsp to create our chroot
-    exec { "build-$image" :
-        command => "/usr/sbin/ltsp-build-client --arch amd64 --chroot $image",
+    exec { "build-${image}" :
+        command => "/usr/sbin/ltsp-build-client --arch amd64 --chroot ${image}",
         creates => $chroot,
         timeout => 0, # This will take a while so turnoff timeout
     } ->
 
     # Now we'll mount proc
-    exec { "mount-proc-$image" :
-        command => "/bin/mount -t proc proc $chroot/proc",
-        unless  => "/bin/mountpoint -q $chroot/proc || /usr/bin/dpkg -l $packages",
+    exec { "mount-proc-${image}" :
+        command => "/bin/mount -t proc proc ${chroot}/proc",
+        unless  => "/bin/mountpoint -q ${chroot}/proc || /usr/bin/dpkg -l ${packages}",
     } ->
 
     # Download puppet-bootstrapper.bash
-    exec { "download-bootstrapper-$image" :
-        command => "/usr/bin/wget -O $chroot/root/puppet-bootstrapper.bash https://raw.githubusercontent.com/jacksonargo/puppet-bootstrapper/master/puppet-bootstrapper.bash",
-        creates => "$chroot/root/puppet-bootstrapper.bash"
+    exec { "download-bootstrapper-${image}" :
+        command => "/usr/bin/wget -O ${chroot}/root/puppet-bootstrapper.bash https://raw.githubusercontent.com/jacksonargo/puppet-bootstrapper/master/puppet-bootstrapper.bash",
+        creates => "${chroot}/root/puppet-bootstrapper.bash"
     } ->
 
     # Install puppet on the client
-    exec { "install-puppet-$image" :
-        command => "/usr/sbin/chroot $chroot /bin/bash /root/puppet-bootstrapper.bash",
-        creates => "$chroot/usr/bin/puppet",
-        environment => ["MYCERT=$image_cert", "PMCERT=$image_puppetmaster", "PMADDR=$image_puppetmasterip", "ENVIRO=$image_environment"],
-        timeout => 0
+    exec { "install-puppet-${image}" :
+        command     => "/usr/sbin/chroot ${chroot} /bin/bash /root/puppet-bootstrapper.bash",
+        creates     => "${chroot}/usr/bin/puppet",
+        environment => ["MYCERT=${image_cert}", "PMCERT=${image_puppetmaster}", "PMADDR=${image_puppetmasterip}", "ENVIRO=${image_environment}"],
+        timeout     => 0
     } ->
 
     # Run puppet in the chroot
-    exec { "run-puppet-$image" : 
-        command => "/usr/sbin/chroot $chroot /usr/bin/puppet agent --test",
+    exec { "run-puppet-${image}" :
+        command => "/usr/sbin/chroot ${chroot} /usr/bin/puppet agent --test",
         timeout => 0
     } ~>
 
     # Update the image
-    exec { "update-$image" :
-        command => "/usr/sbin/ltsp-update-image --arch $image",
+    exec { "update-${image}" :
+        command     => "/usr/sbin/ltsp-update-image --arch ${image}",
         refreshonly => true
     }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,22 @@
-/* init.pp
- *
- * Startup for the ltsp module
- * The ltsp module install ltsp and creates the images for each server
- */
+# init.pp
+#
+# Startup for the ltsp module
+# The ltsp module install ltsp and creates the images for each server
+#
 class ltsp ($package) {
 
     # Install the ltsp packages
     package { $package : ensure => installed }
     
     # Configure the server
-    create_resources(file, hiera_hash('ltsp::files'), hiera_hash('ltsp::file_defaults'))
+    create_resources(file, hiera_hash('ltsp::files'),
+        hiera_hash('ltsp::file_defaults'))
 
     # Create the ltsp client images
-    create_resources(ltsp::image, hiera_hash('ltsp::images'), hiera_hash('ltsp::image_defaults'))
+    create_resources(ltsp::image, hiera_hash('ltsp::images'),
+        hiera_hash('ltsp::image_defaults'))
 
-    Package[$package] -> File <| tag == 'ltsp' |> -> Ltsp::Client <| tag == 'ltsp' |>
+    Package[$package] ->
+    File <| tag == 'ltsp' |> ->
+    Ltsp::Client <| tag == 'ltsp' |>
 }
